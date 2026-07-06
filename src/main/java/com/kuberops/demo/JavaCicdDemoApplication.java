2be1aa4366eb7ed104f66eb172b3fedad755baab
Neelima Setti <neelimasettioff@gmail.com>
2026-07-06 18:02:39 +0000

    Initial Spring Boot CI/CD application

---
+++ src/main/java/com/kuberops/demo/JavaCicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kuberops.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCicdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuberops/demo/JavaCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuberops.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
